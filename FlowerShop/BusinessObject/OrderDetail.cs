8fd5ab874a9096e8c3ed132a46adae71d9fbc76c
QuyTurtle12 <Ngocquyquy29@gmail.com>
2024-01-26 10:21:02 +0700

    Basic Structure of Project
    You guys can work base on this structure from now on.
    Note: I haven't add dotnet package yet.

---
+++ FlowerShop/BusinessObject/OrderDetail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    internal class OrderDetail
+    {
+        public required string OrderID {  get; set; }
+        public required string FlowerID { get; set; }
+        public int Amount { get; set; }
+        public double Price { get; set; } //Unit price x Amount
+
+        public OrderDetail(string orderID, string flowerID, int amount, double price)
+        {
+            OrderID = orderID;
+            FlowerID = flowerID;
+            Amount = amount;
+            Price = price;
+        }
+
+        public OrderDetail() { }
+
+        public override string ToString()
+        {
+            return $"OrderID:{OrderID}|FlowerID:{FlowerID}|Amount:{Amount}|Price:{Price}";
+        }
+    }
+}
