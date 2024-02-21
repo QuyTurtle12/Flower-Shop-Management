cd24f9e6a696baaa959997af3e0ba5d3f5daad3e
QuyTurtle12 <Ngocquyquy29@gmail.com>
2024-02-21 10:16:58 +0700

    Nothing

--- FlowerShop/GUI/obj/Debug/net8.0-windows/GUI.AssemblyInfo.cs
+++ FlowerShop/GUI/obj/Debug/net8.0-windows/GUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc3b5313c66064c0f5d571df5416c2cd54096446")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
