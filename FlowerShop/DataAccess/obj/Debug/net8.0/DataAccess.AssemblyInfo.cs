dac32ea09801ebac848226634c89d33fa3026a80
hoaphamse171730 <hoappse.171730@fpt.edu.vn>
2024-03-15 17:15:11 +0700

    Just update a logic flow of "Update Order Status"

--- FlowerShop/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ FlowerShop/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6057828a160166e5273d09e79dcac5dac2abb1e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c77e4f1dd2b336864fdc7c320b7e1a7623e3bf19")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
